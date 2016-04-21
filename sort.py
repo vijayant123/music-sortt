a6cafd8c2d8ed6b9bd07e4f8823920cd33834a53
vijayant <vijayant.saini123@gmail.com>
2016-04-21 11:50:16 +0530

    wrap up

--- sort.py
+++ sort.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+
+import subprocess
 from os import listdir
 import os, sys
 from os.path import isfile, join
@@ -6,16 +8,18 @@ from pygame import mixer
 
 mypath = "/home/error-404/Desktop/songs"
 savepath = "/home/error-404/Desktop/dance"
+altpath = "/home/error-404/Desktop/others"
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 mixer.init()
 
+flag = 0
+
 for f in onlyfiles:
     print f
     mixer.music.load(join(mypath,f))
     mixer.music.play(0, 40)
-    print mixer.music.get_pos()
     while True:
         a = raw_input()
         if(a == 'q'):
@@ -24,9 +28,20 @@ for f in onlyfiles:
             mixer.music.rewind()
             mixer.music.play(0, int(a)*10)
         elif(a == ''):
+            mixer.music.stop()
+            os.rename(join(mypath, f), join(altpath, f))
             break;
         elif(a == '/'):
             # t'is the chosen ONE!
             # move it to a secure location
+            mixer.music.stop()
             os.rename(join(mypath, f), join(savepath, f))
             break;
+        elif(a == 'p'):
+            if(flag == 0):
+                mixer.music.pause()
+                flag = 1
+            else:
+                mixer.music.unpause()
+                flag = 0
+
